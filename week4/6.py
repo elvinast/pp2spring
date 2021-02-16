662abbd968446764c3371a462dc80237b074a6f3
Elvina <elvinashm@gmail.com>
2021-02-17 00:17:18 +0600

    new problem added

---
+++ week4/6.py
@@ -0,0 +1,19 @@
+d = {}
+sInput = []
+f = []
+import sys
+sInput = (str(sys.stdin.read()).split()) #чтобы считывать все
+for i in sInput:
+    if i in d.keys():
+        d[i] += 1
+    else: d[i] = 1
+
+def checking(x):
+    return(-x[0], x[1])  
+
+
+for key, value in d.items():
+	f.append((value, key))
+f.sort(key=checking)
+for i in f:
+    print(i[1])
